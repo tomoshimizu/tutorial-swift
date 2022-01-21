fd44c456ae87736a042ba4fc011556d11f23d811
tomoshimizu <tomo.shimizu@PC-314.local>
2022-01-21 21:34:15 +0900

    add: Closure

---
+++ Functions & Named Types/closure.playground/Contents.swift
@@ -0,0 +1,33 @@
+// MARK: - Closure
+
+typealias Operate = (Int, Int) -> Int
+
+func add(a: Int, b: Int) -> Int {
+  a + b
+}
+
+func printResult(_ operate: Operate, _ a: Int, _ b: Int) {
+  let result = operate(a, b)
+  print(result)
+}
+
+var operate = add
+var addClosure = { (a: Int, b: Int) -> Int in
+    a + b
+}
+
+add(a: 7, b: 3)
+operate(6, 11)
+addClosure(6, 11)
+
+printResult(add, 6, 11)
+printResult(operate, 6, 11)
+printResult(addClosure, 6, 11)
+printResult(+, 6, 11)
+
+printResult(
+    { (a: Int, b: Int) -> Int in
+        a * b + 100
+    },
+    3, 10
+)
